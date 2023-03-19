7d10cb7fd6ca63532d9c69bf2994fa2387130c6e
KitaharaMugiro <you@example.com>
2023-03-19 11:14:35 +0900

    小文字に戻す

--- functions/src/service/TokenService.ts
+++ functions/src/service/TokenService.ts
@@ -3,8 +3,9 @@ import { v4 as uuidv4 } from "uuid"
 import * as admin from "firebase-admin"
 
 export class TokenService {
-  private static TopCollectionName = "Contract"
-  private static TopCollectionNameTestMode = "Contract-Test"
+  private static TopCollectionName = "contract"
+  private static TopCollectionNameTestMode = "contract-test"
+  private static walletAddressName = "walletAddress"
 
   private static getDB(testMode: boolean) {
     const firestore = admin.firestore()
@@ -16,9 +17,6 @@ export class TokenService {
   }
 
   private static verifySignature(address: string, signature: string) {
-    if (!address) throw new Error("Authentication error: Address is not supplied.")
-    if (!signature) throw new Error("Authentication error: Signature is not supplied.")
-
     // Verify the signature using ethers.js
     const message = address
     const recoveredAddress = ethers.verifyMessage(message, signature)
@@ -37,17 +35,17 @@ export class TokenService {
     if (!testMode) this.verifySignature(walletAddress, signature)
 
     // Generate a new UUID for the token address
-    const TokenAddress = uuidv4()
+    const tokenAddress = uuidv4().replace(/-/g, "").toLowerCase()
 
     // Save the owner address to Firestore
-    const OwnerAddress = walletAddress.toLowerCase() // Ensure the address is in lowercase
+    const ownerAddress = walletAddress.toLowerCase() // Ensure the address is in lowercase
 
-    await this.getDB(testMode).doc(TokenAddress).set({
-      OwnerAddress: OwnerAddress,
+    await this.getDB(testMode).doc(tokenAddress).set({
+      ownerAddress,
     })
 
     // Return the token address
-    return { tokenAddress: TokenAddress }
+    return { tokenAddress }
   }
 
   static async migrate(
@@ -62,14 +60,13 @@ export class TokenService {
 
     const db = this.getDB(testMode)
     const tokenData = await this.getTokenData(tokenAddress, db)
-
     this.validateOwnership(walletAddress, tokenData)
+
+    const { walletAddressList, amountList } = await this.getWalletAddressAndAmounts(tokenAddress, db)
 
     const { abi, bytecode } = await this.getContractData()
     const provider = this.getProvider()
     const privateKey = process.env.PRIVATE_KEY || "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-
-    const { walletAddressList, amountList } = await this.getWalletAddressAndAmounts(tokenAddress, db)
 
     const wallet = new ethers.Wallet(privateKey, provider)
     const factory = new ethers.ContractFactory(abi, bytecode, wallet)
@@ -89,17 +86,16 @@ export class TokenService {
     const tokenData = tokenDoc.data()
 
     if (!tokenData) {
-      throw new Error("Authentication error: You are not allowed to execute this method.")
+      throw new Error("Authentication error: You are not allowed to execute this method.(1)")
     }
 
     return tokenData
   }
 
   static validateOwnership(walletAddress: string, tokenData: admin.firestore.DocumentData) {
-    const ownerAddress = tokenData.OwnerAddress
-
+    const ownerAddress = tokenData.ownerAddress
     if (ownerAddress !== walletAddress.toLowerCase() || tokenData.web3ContractAddress) {
-      throw new Error("Authentication error: You are not allowed to execute this method.")
+      throw new Error("Authentication error: You are not allowed to execute this method.(2)")
     }
   }
 
@@ -122,7 +118,7 @@ export class TokenService {
 
     await db
       .doc(tokenAddress.toLowerCase())
-      .collection("WalletAddress")
+      .collection(this.walletAddressName)
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
@@ -132,7 +128,7 @@ export class TokenService {
 
     const walletAddressList = Object.keys(addressData)
     const amountList = walletAddressList.map((walletAddress) => {
-      return addressData[walletAddress].Amount
+      return addressData[walletAddress].amount
     })
 
     return { walletAddressList, amountList }
