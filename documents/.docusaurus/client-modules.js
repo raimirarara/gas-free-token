0c52abd7c9dc098c68d7036b46732c9507feef51
Rai <raimi.rarara@gmail.com>
2023-03-19 19:43:01 +0900

    delete documents

--- documents/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/raimiura/DApps/gas-free-token/documents/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/raimiura/DApps/gas-free-token/documents/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/raimiura/DApps/gas-free-token/documents/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/raimiura/DApps/gas-free-token/documents/src/css/custom.css'),
-];
