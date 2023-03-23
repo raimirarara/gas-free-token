ce9df8e69e8845f045c1194b2cd3d77a83e169df
raimirarara <raimi.rarara@gmail.com>
2023-03-24 03:26:51 +0900

    edit sidebar

--- documents/docs/intro.md
+++ documents/docs/intro.md
@@ -4,50 +4,23 @@ sidebar_position: 1
 
 # Overview
 
-Have you ever faced these problems when issuing tokens?
+Are you struggling with these challenges when issuing tokens?
 
-- **No knowledge of blockchain**
-- **Gas costs**
-- **Processing is slow**
+- **Lack of blockchain knowledge**
+- **High gas costs**
+- **Slow processing times**
 
-It costs a lot of gas and is clearly over-engineered to handle tokens that have no value yet on web3. It is like keeping your children's banknotes in a vault.
+Managing tokens with no current value on web3 can be inefficient and expensive in terms of gas fees. It's like storing your child's play money in a high-security vault.
 
-## Gas Free Token
+## Introducing Gas-Free Tokens
 
-Gas Free Token is a token that can be used without the need to pay for gas.
-
-Gas Free Token is a temporary Token that is used on web2. Later, when the token starts to gain value, it can be transferred to web3.
+Gas-Free Tokens are designed for use without the need to pay for gas fees. These temporary tokens can be utilized on web2 until they gain value, at which point they can be transferred to web3.
 
 ### What you'll need
 
-- [Node.js](https://nodejs.org/en/download/) version 16.14 or above:
-  - When installing Node.js, you are recommended to check all checkboxes related to dependencies.
+To get started with Gas-Free Tokens, you will need:
 
-## Generate a new site
-
-Generate a new Docusaurus site using the **classic template**.
-
-The classic template will automatically be added to your project after you run the command:
-
-```bash
-npm init docusaurus@latest my-website classic
-```
-
-You can type this command into Command Prompt, Powershell, Terminal, or any other integrated terminal of your code editor.
-
-The command also installs all necessary dependencies you need to run Docusaurus.
-
-## Start your site
-
-Run the development server:
-
-```bash
-cd my-website
-npm run start
-```
-
-The `cd` command changes the directory you're working with. In order to work with your newly created Docusaurus site, you'll need to navigate the terminal there.
-
-The `npm run start` command builds your website locally and serves it through a development server, ready for you to view at http://localhost:3000/.
-
-Open `docs/intro.md` (this page) and edit some lines: the site **reloads automatically** and displays your changes.
+- For frontend usage or to follow a tutorial:
+  - [MetaMask](https://metamask.io/download/)
+- For backend usage:
+  - [ethers.js](https://docs.ethers.org/v5/getting-started/#installing)
--- documents/docusaurus.config.js
+++ documents/docusaurus.config.js
@@ -43,14 +43,14 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        // },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
@@ -76,9 +76,9 @@ const config = {
             position: "left",
             label: "Tutorial",
           },
-          { to: "/blog", label: "Blog", position: "left" },
+
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/raimirarara/gas-free-token",
             label: "GitHub",
             position: "right",
           },
@@ -96,38 +96,18 @@ const config = {
               },
             ],
           },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
+
           {
             title: "More",
             items: [
               {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/raimirarara/gas-free-token",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} GasFreeToken.Inc`,
       },
       prism: {
         theme: lightCodeTheme,
