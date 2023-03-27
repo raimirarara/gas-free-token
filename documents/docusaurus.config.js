cc59cd6b0ab729ec784b74fa574e2dad4ccc4613
raimirarara <raimi.rarara@gmail.com>
2023-03-28 03:22:34 +0900

    delete docusaurus img

--- documents/docusaurus.config.js
+++ documents/docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/img/gas-free-token_logo.png",
       navbar: {
         title: "Gas-Free Token",
         logo: {
